df16ea1bf2ae64cd0cfe77e97e55a8be08ff9291
CGAN23 <82013269+CGAN23@users.noreply.github.com>
2021-04-07 15:52:44 +0800

    Main script

---
+++ script.js
@@ -0,0 +1,7 @@
+let message1 = `“All our dreams can come true, if we have the courage to pursue them.” – Walt Disney`;
+let message2 = `“The secret of getting ahead is getting started.” – Mark Twain`;
+let message3 = `“Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve.” – Mary Kay Ash`;
+let message4 = `“The best time to plant a tree was 20 years ago. The second best time is now.” – Chinese Proverb`;
+let message5 = `“If people are doubting how far you can go, go so far that you can’t hear them anymore.” – Michele Ruiz`;
+
+let inspoMessages = [message1, message2, message3, message4,message5];
